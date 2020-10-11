1a73b146a68c20ce7294f7fdb92a2e47a8f0f248
Himanshuranjan30 <himanshuranjan30@gmail.com>
2020-10-11 10:50:48 +0530

    tweak

--- lib/sign/register.dart
+++ lib/sign/register.dart
@@ -40,76 +40,78 @@ class _RegisterState extends State<Register> {
     return Scaffold(
       appBar: AppBar(
           title: Text('Student Registration'), backgroundColor: Colors.indigo),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.network(
-              'https://www.majhimaitrin.in/img/studlogin.png',
-              height: 100,
-              width: 100,
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Text(
-              'Enter Your SRN',
-              style: TextStyle(
-                  fontStyle: FontStyle.normal, fontWeight: FontWeight.bold),
-            ),
-            Center(
-              child: Container(
-                width: 250,
-                child: TextField(
-                  controller: _srn,
-                  decoration: InputDecoration(hintText: 'enter your srn'),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(
+                'https://www.majhimaitrin.in/img/studlogin.png',
+                height: 100,
+                width: 100,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                'Enter Your SRN',
+                style: TextStyle(
+                    fontStyle: FontStyle.normal, fontWeight: FontWeight.bold),
+              ),
+              Center(
+                child: Container(
+                  width: 250,
+                  child: TextField(
+                    controller: _srn,
+                    decoration: InputDecoration(hintText: 'enter your srn'),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Choose a Password',
-              style: TextStyle(
-                  fontStyle: FontStyle.normal, fontWeight: FontWeight.bold),
-            ),
-            Center(
-              child: Container(
-                width: 250,
-                child: TextField(
-                  controller: _pass,
-                  decoration: InputDecoration(hintText: 'Enter password'),
+              SizedBox(height: 20),
+              Text(
+                'Choose a Password',
+                style: TextStyle(
+                    fontStyle: FontStyle.normal, fontWeight: FontWeight.bold),
+              ),
+              Center(
+                child: Container(
+                  width: 250,
+                  child: TextField(
+                    controller: _pass,
+                    decoration: InputDecoration(hintText: 'Enter password'),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            FlatButton.icon(
-              onPressed: () => onsave(),
-              label: Text('Register', style: TextStyle(color: Colors.white)),
-              icon: Icon(Icons.app_registration),
-              color: Colors.indigo,
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Text(
-              'Already Have an Account?',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            FlatButton.icon(
-              onPressed: () => Navigator.of(context).pushNamed('/login'),
-              label: Text(
-                'Login',
-                style: TextStyle(color: Colors.white),
+              SizedBox(
+                height: 20,
               ),
-              icon: Icon(Icons.login),
-              color: Colors.indigo,
-            )
-          ],
+              FlatButton.icon(
+                onPressed: () => onsave(),
+                label: Text('Register', style: TextStyle(color: Colors.white)),
+                icon: Icon(Icons.app_registration),
+                color: Colors.indigo,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                'Already Have an Account?',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              FlatButton.icon(
+                onPressed: () => Navigator.of(context).pushNamed('/login'),
+                label: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white),
+                ),
+                icon: Icon(Icons.login),
+                color: Colors.indigo,
+              )
+            ],
+          ),
         ),
       ),
     );
