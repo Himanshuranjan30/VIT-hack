638b084f727929848b79a168fd22aff7d8263376
Himanshuranjan30 <himanshuranjan30@gmail.com>
2020-10-11 10:23:32 +0530

    charts added

--- lib/status.dart
+++ lib/status.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:mongo_dart/mongo_dart.dart' as mongo;
 import "dart:convert";
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:intl/intl.dart';
 
 class Status extends StatefulWidget {
   String uid;
@@ -20,6 +22,9 @@ class _StatusState extends State<Status> {
   List<String> classh = [];
   List<String> study = [];
   List<String> sleep = [];
+  List<String> dates = [];
+  List<double> perf = [];
+  bool ischart = false;
   String performance = 'Calculating your Performance..';
   database() async {
     //final User user = await _auth.currentUser;
@@ -36,12 +41,47 @@ class _StatusState extends State<Status> {
         if (v['performance'] != null) performance = v["performance"].toString();
       });
     });
+    await coll.find().forEach((v) {
+      classh.add(v["classhours"]);
+      sleep.add(v["sleephours"]);
+      study.add(v["studyhours"]);
+      dates.add(v["_id"]);
+      perf.add(v["performance"]);
+    });
+    setState(() {
+      ischart = true;
+    });
+    print(classh);
+    print(sleep);
+    print(study);
+    print(dates);
+    print(perf);
   }
 
   void initState() {
     // ignore: deprecated_member_use
     super.initState();
     database();
+  }
+
+  List<charts.Series<Sales, String>> _createSampleData() {
+    final data = [
+      new Sales('Monday', perf[0] != null ? perf[0].toInt() : 0),
+      new Sales('Tuesday', perf[1] != null ? perf[1].toInt() : 0),
+      new Sales('Wednesday', perf[2] != null ? perf[2].toInt() : 0),
+      new Sales('Thursday', perf[3] != null ? perf[3].toInt() : 0),
+      new Sales('Friday', perf[4] != null ? perf[4].toInt() : 0)
+    ];
+
+    return [
+      new charts.Series<Sales, String>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (Sales sales, _) => sales.date,
+        measureFn: (Sales sales, _) => sales.hours,
+        data: data,
+      )
+    ];
   }
 
   @override
@@ -51,53 +91,69 @@ class _StatusState extends State<Status> {
           title: Text('Performance Report'),
           backgroundColor: Colors.indigo,
         ),
-        body: Column(
-          children: [
-            Image.network(
-                'https://www.cambridgemaths.org/Images/The-trouble-with-graphs.jpg',
-                height: 150,
-                width: 200),
-            Text(
-              'Performance Report',
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic),
-            ),
-            SizedBox(
-              height: 140,
-            ),
-            Center(
-              child: Text(
-                'Your Performance Cofficient:',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.network(
+                  'https://www.cambridgemaths.org/Images/The-trouble-with-graphs.jpg',
+                  height: 150,
+                  width: 200),
+              Text(
+                'Performance Report',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic),
               ),
-            ),
-            Center(
-              child: Text(
-                performance.toString(),
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              SizedBox(
+                height: 140,
               ),
-            ),
-            SizedBox(
-              height: 250,
-            ),
-            performance == 'Calculating your Performance..'
-                ? SpinKitRotatingCircle(
-                    color: Colors.blue,
-                    size: 50.0,
-                  )
-                : Container(),
-            SizedBox(
-              height: 15,
-            ),
-            Center(
-              child: Text(
-                '(If not shown Kindly re-enter the data)',
-                style: TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
+              Center(
+                child: Text(
+                  'Your Performance Cofficient:',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
               ),
-            )
-          ],
+              Center(
+                child: Text(
+                  performance.toString(),
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+              ischart == true
+                  ? Container(
+                      height: 200,
+                      width: 400,
+                      child: charts.BarChart(_createSampleData()))
+                  : Container(),
+              performance == 'Calculating your Performance..'
+                  ? SpinKitRotatingCircle(
+                      color: Colors.blue,
+                      size: 50.0,
+                    )
+                  : Container(),
+              SizedBox(
+                height: 15,
+              ),
+              Text('Performance Graph(X- Day, Y- Performance Cofficient'),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: Text(
+                  '(If not shown Kindly re-enter the data)',
+                  style: TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
+                ),
+              )
+            ],
+          ),
         ));
   }
+}
+
+class Sales {
+  String date;
+  int hours;
+
+  Sales(this.date, this.hours);
 }
